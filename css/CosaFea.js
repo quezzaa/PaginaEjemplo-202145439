2a39027ab5d31f1d5ed0099ef0da0c700394a0d3
quezzaa <quezadita0409@gmail.com>
2023-10-24 20:03:10 -0600

    Pagina Web - David

---
+++ css/CosaFea.js
@@ -0,0 +1,4 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-main ul.nav-menu").classList.toggle("visible");
+    console.log("Si funciona esa cosa fea (No me gusta Js)")
+});
